902a23afb3398048c51010575f5ed8d611eeeba0
Ragnar Pálsson <raggipals@gmail.com>
2023-06-28 13:44:29 +0000

    Finished exercise 2.11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from 'axios'
 
 
 
@@ -12,19 +13,21 @@ const Header = ({ text }) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'James Bond', number: '007', id: 1},
-        { name: 'Arto Hellas', number: '040-123456', id: 2 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 3 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 4 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 5 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('Your name')
     const [newNumber, setNewNumber] = useState('Your number')
     const [filterValue, setFilterValue] = useState('')
     const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
 
+    const hook = () => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }
 
+    useEffect(hook, [])
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
